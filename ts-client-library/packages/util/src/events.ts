0dbaee05d26eb71d2ab673afc9f3645569d14be1
Connor <connor@pop-os.localdomain>
2021-03-23 16:34:48 -0400

    Remove ts-client-library

--- ts-client-library/packages/util/src/events.ts
+++
@@ -1,9 +0,0 @@
-export interface EventListener<T = Event> {
-	(evt: T): void
-}
-
-export interface EventListenerObject<T = Event> {
-	handleEvent(evt: T): void
-}
-
-export type EventListenerOrEventListenerObject<T = Event> = EventListener<T> | EventListenerObject<T>
