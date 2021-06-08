2431a1eebd26822b6a2b3471965755f8ae15febf
Jesus Espitia <jesus@opacity.io>
2021-06-07 18:19:00 -0700

    DEV-22: Update text on Homepage

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -46,69 +46,93 @@ const LandingPage = ({ history }) => {
   };
 
   return (
-    <SiteWrapper showSignUpModal={showSignUpModal} handleCloseSignUpModal={handleCloseSignUpModal} isHome={true} history={history}>
-      <div className='first-ele'>
-        <div className='container-xl' data-aos='fade-up'>
-          <div className='row'>
-            <div className='col-md-6'>
+    <SiteWrapper
+      showSignUpModal={showSignUpModal}
+      handleCloseSignUpModal={handleCloseSignUpModal}
+      isHome={true}
+      history={history}
+    >
+      <div className="first-ele">
+        <div className="container-xl" data-aos="fade-up">
+          <div className="row">
+            <div className="col-md-6">
               <h1>Privacy By Design</h1>
-              <div className='description mb-5 mt-2'>Free to share ideas. Free to be private. Free to be you.</div>
-              <div className='row mb-3'>
-                <div className='col-md-5'>
-                  <button className='btn btn-pill btn-cart'>
+              <div className="description mb-5 mt-2">
+                Free to share ideas. Free to be private. Free to be you.
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-5">
+                  <button className="btn btn-pill btn-cart">
                     <span></span>
                     Purchase OPCT
                   </button>
                 </div>
-                <div className='col-md-5'>
-                  <button className='btn btn-pill btn-upload' onClick={handleOpenSignUpModal}>
+                <div className="col-md-5">
+                  <button
+                    className="btn btn-pill btn-upload"
+                    onClick={handleOpenSignUpModal}
+                  >
                     <span></span>
                     Get Started for FREE
                   </button>
                 </div>
               </div>
-              <div className='col-md-10 banner-text'>No personal information required. Get 10GB file storage and file sharing for free</div>
+              <div className="col-md-10 banner-text">
+                No personal information required. Get 10GB file storage and file
+                sharing for free
+              </div>
             </div>
-            <div className='col-md-6'>
+            <div className="col-md-6">
               <img src={bannerImage}></img>
-              <img className='d-none' src={opacitym}></img>
+              <img className="d-none" src={opacitym}></img>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-xl second-ele '>
-        <div className='row text-center' data-aos='fade-up'>
-          <div className='col-md-4 card-items image-personal'>
-            <div className='image-ele'></div>
-            <div className='title'>No Personal Info Required</div>
-            <div className='descriptions'>
-              We never ask for your personal information. You're always protected, so you'll never end up on the dark web
+      <div className="container-xl second-ele ">
+        <div className="row text-center" data-aos="fade-up">
+          <div className="col-md-4 card-items image-personal">
+            <div className="image-ele"></div>
+            <div className="title">No Personal Info Required</div>
+            <div className="descriptions">
+              We never ask for your personal information. You're always
+              protected, so you'll never end up on the dark web
             </div>
           </div>
-          <div className='col-md-4 card-items image-share'>
-            <div className='image-ele'></div>
-            <div className='title'>Share Files Your Way</div>
-            <div className='descriptions'>You control who can view your files. By default, you and only you even know these files exist.</div>
+          <div className="col-md-4 card-items image-share">
+            <div className="image-ele"></div>
+            <div className="title">Share Files Your Way</div>
+            <div className="descriptions">
+              You control who can view your files. By default, you and only you
+              even know these files exist.
+            </div>
           </div>
-          <div className='col-md-4 card-items image-pay'>
-            <div className='image-ele'></div>
-            <div className='title'>Pay Using Cryptocurrency</div>
-            <div className='descriptions'>Using the OPCT token, you can pay for your storage needs without ever having to use a credit card.</div>
+          <div className="col-md-4 card-items image-pay">
+            <div className="image-ele"></div>
+            <div className="title">Pay Using Cryptocurrency</div>
+            <div className="descriptions">
+              Using the OPCT token, you can pay for your storage needs without
+              ever having to use a credit card.
+            </div>
           </div>
         </div>
-        <div className='row how-it-works' style={{ marginTop: 150 }}>
-          <div className='col-md-12' data-aos='fade-up'>
-            <div className='row justify-content-center'>
-              <div className='col-md-5 pr-4'>
-                <img className='mt-4' src={dashboard} />
+        <div className="row how-it-works" style={{ marginTop: 150 }}>
+          <div className="col-md-12" data-aos="fade-up">
+            <div className="row justify-content-center">
+              <div className="col-md-5 pr-4">
+                <img className="mt-4" src={dashboard} />
               </div>
-              <div className='col-md-5 pl-4'>
-                <h1 className='title mb-4'>Share any private file easily, with anyone, on any device.</h1>
-                <div className='descriptions'>
-                  <p className='mb-4'>
-                    File sharing with Opacity is quick and simple. By just sharing a link, you can send anything – from photos and videos to zipped
-                    folders and large CAD files – with anyone, even if they don’t have a Opacity account. Share links and files from anywhere, using
-                    your phone, tablet or computer.
+              <div className="col-md-5 pl-4">
+                <h1 className="title mb-4">
+                  Share any private file easily, with anyone, on any device.
+                </h1>
+                <div className="descriptions">
+                  <p className="mb-4">
+                    File sharing with Opacity is quick and simple. By just
+                    sharing a link, you can send anything – from photos and
+                    videos to zipped folders and large CAD files – with anyone,
+                    even if they don’t have a Opacity account. Share links and
+                    files from anywhere, using your phone, tablet or computer.
                   </p>
                   {/* <p>
                     And unlike email attachments, you don’t have to worry about file size limits—share files as large as your cloud storage space
@@ -119,52 +143,68 @@ const LandingPage = ({ history }) => {
             </div>
           </div>
 
-          <div className='col-md-12 dot-left'></div>
+          <div className="col-md-12 dot-left"></div>
 
-          <div className='col-md-12' data-aos='fade-up'>
-            <div className='row justify-content-center'>
-              <div className='col-md-5 pr-4'>
-                <h1 className='title mb-4'>Get real-time updates when you share files</h1>
-                <div className='descriptions'>
-                  <p className='mb-4'>No Personal Information Means 100% Trust</p>
-                  <p className='mb-4'>
-                    Only Opacity can guarantee your information will never be leaked or used by 3rd party ads. Because we don't ask for it! Your name,
-                    address, or credit card will never be compromised. We respect your privacy by keeping you out of the transaction.
+          <div className="col-md-12" data-aos="fade-up">
+            <div className="row justify-content-center">
+              <div className="col-md-5 pr-4">
+                <h1 className="title mb-4">
+                  No Personal Information Means 100% Trust
+                </h1>
+                <div className="descriptions">
+                  <p className="mb-4">
+                    Only Opacity can guarantee your information will never be
+                    leaked or used by 3rd party ads. Because we don't ask for
+                    it! Your name, address, or credit card will never be
+                    compromised. We respect your privacy by keeping you out of
+                    the transaction.
                   </p>
                 </div>
               </div>
-              <div className='col-md-5 pl-4'>
-                <img className='mt-4' src={share} />
+              <div className="col-md-5 pl-4">
+                <img className="mt-4" src={share} />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='third-ele text-center' data-aos='fade-up'>
-        <div className='container-xl'>
+      <div className="third-ele text-center" data-aos="fade-up">
+        <div className="container-xl">
           <h1>What can Opacity help you do?</h1>
           <h3>
-            Not only can you enjoy 100% private data storage, we use client side end-to-end encryption you won’t find on most other file hosting
-            sites. We’re always expanding our capabilities to remain one step ahead of our competitors, making our file upload system the best choice
-            for your day to day needs
+            Not only can you enjoy 100% private data storage, we use client side
+            end-to-end encryption you won’t find on most other file hosting
+            sites. We’re always expanding our capabilities to remain one step
+            ahead of our competitors, making our file upload system the best
+            choice for your day to day needs
           </h3>
-          <button className='btn btn-pill btn-primary' onClick={handleOpenSignUpModal}>
+          <button
+            className="btn btn-pill btn-primary"
+            onClick={handleOpenSignUpModal}
+          >
             Get Started for Free
           </button>
         </div>
       </div>
-      <div className='fourth-ele' data-aos='fade-up'>
-        <div className='container-xl'>
-          <div className='row justify-content-center'>
-            <div className='col-md-6 pr-4 mt-7'>
+      <div className="fourth-ele" data-aos="fade-up">
+        <div className="container-xl">
+          <div className="row justify-content-center">
+            <div className="col-md-6 pr-4 mt-7">
               <h1>Your Files, Safe and Secure.</h1>
               <h4>
-                Opacity uses state-of-the-art encryption algorithms to ensure that your files are secure. The Opacity platform encrypts your files at
-                rest to provide comprehensive protection for your files. As long as you protect your Opacity Handle, your data is safe.
+                Opacity uses state-of-the-art encryption algorithms to ensure
+                that your files are secure. The Opacity platform encrypts your
+                files at rest to provide comprehensive protection for your
+                files. As long as you protect your Opacity Handle, your data is
+                safe.
               </h4>
             </div>
-            <div className='col-md-6 pl-4'>
-              <Lottie options={defaultOptions} width={"100%"} isClickToPauseDisabled/>
+            <div className="col-md-6 pl-4">
+              <Lottie
+                options={defaultOptions}
+                width={"100%"}
+                isClickToPauseDisabled
+              />
               {/* <img src={secure} />
               <img className="d-none" src={securem} /> */}
               {/* <div className="circle-primary"></div> */}
@@ -172,50 +212,63 @@ const LandingPage = ({ history }) => {
           </div>
         </div>
       </div>
-      <div className='six-ele' data-aos='fade-up'>
-        <div className='container-xl'>
-          <div className='row justify-content-center'>
-            <div className='col-md-6 pr-4 mt-7'>
-              <Lottie options={defaultOptions2} width={"100%"} isClickToPauseDisabled/>
+      <div className="six-ele" data-aos="fade-up">
+        <div className="container-xl">
+          <div className="row justify-content-center">
+            <div className="col-md-6 pr-4 mt-7">
+              <Lottie
+                options={defaultOptions2}
+                width={"100%"}
+                isClickToPauseDisabled
+              />
 
               {/* <img src={rules} />
               <img className="d-none" src={rulesm} />
               <div className="circle-yellow"></div> */}
             </div>
-            <div className='col-md-6 pl-4 '>
+            <div className="col-md-6 pl-4 ">
               <h1>Your Handle, Your Rules.</h1>
               <h4>
-                Your unique Opacity Account Handle is the single point of access to your storage account. Like a private encryption key,
-                only you know this Handle, and only you have access to your files unless you decide to share the Handle.
-                You may also choose to only share individual files with a unique File Handle that others may use to privately download or view files on the Opacity platform.
+                Your unique Opacity Account Handle is the single point of access
+                to your storage account. Like a private encryption key, only you
+                know this Handle, and only you have access to your files unless
+                you decide to share the Handle. You may also choose to only
+                share individual files with a unique File Handle that others may
+                use to privately download or view files on the Opacity platform.
               </h4>
             </div>
           </div>
         </div>
       </div>
-      <div className='third-ele text-center' data-aos='fade-up'>
-        <div className='container-xl'>
+      <div className="third-ele text-center" data-aos="fade-up">
+        <div className="container-xl">
           <h1>Sync files automatically</h1>
-          <h3 className='pl-5 pr-5'>
-            When you download the Opacity desktop app for Windows or Mac, you can automatically sync your local files with your Opacity Private cloud
-            storage
+          <h3 className="pl-5 pr-5">
+            When you download the Opacity desktop app for Windows or Mac, you
+            can automatically sync your local files with your Opacity Private
+            cloud storage
           </h3>
-          <button className='btn btn-pill btn-primary' onClick={handleOpenSignUpModal}>
+          <button
+            className="btn btn-pill btn-primary"
+            onClick={handleOpenSignUpModal}
+          >
             Get Started for Free
           </button>
         </div>
       </div>
-      <div className='card-items container-xl' data-aos='fade-up'>
+      <div className="card-items container-xl" data-aos="fade-up">
         <h1>More Info? No Problem.</h1>
-        <div className='row' style={{ justifyContent: 'space-around' }}>
-          <div className='col-md-3 '>
-            <div className='card-item'>
-              <div className='rectangle'>
+        <div className="row" style={{ justifyContent: "space-around" }}>
+          <div className="col-md-3 ">
+            <div className="card-item">
+              <div className="rectangle">
                 <img src={community} />
               </div>
               <h2> Community for Opacity</h2>
               <h3>
-                We are a group of privacy enthusiasts looking to build a truly private data storage solution. Take a look at some of the applications our community has created
+                We are a group of privacy enthusiasts looking to build a truly
+                private data storage solution. Take a look at some of the
+                applications our community has created
               </h3>
               <NavLink>READ MORE</NavLink>
             </div>
@@ -233,48 +286,52 @@ const LandingPage = ({ history }) => {
               <NavLink>READ MORE</NavLink>
             </div>
           </div> */}
-          <div className='col-md-3 '>
-            <div className='card-item'>
-              <div className='rectangle'>
+          <div className="col-md-3 ">
+            <div className="card-item">
+              <div className="rectangle">
                 <img src={stand} />
               </div>
               <h2> Why Opacity Stands Out</h2>
-              <h3>We do things differently than most storage providers. Learn exactly what makes us stand out from the competition.</h3>
+              <h3>
+                We do things differently than most storage providers. Learn
+                exactly what makes us stand out from the competition.
+              </h3>
               <NavLink>READ MORE</NavLink>
             </div>
           </div>
-          <div className='col-md-3 '>
-            <div className='card-item'>
-              <div className='rectangle'>
+          <div className="col-md-3 ">
+            <div className="card-item">
+              <div className="rectangle">
                 <img src={crypto} />
               </div>
               <h2> New to Cryptocurrency?</h2>
               <h3>
-                Cryptocurrency can be a little confusing, but it doesn’t have to be. Take a look at this video to learn how you can get started with
-                cryptocurrency.
+                Cryptocurrency can be a little confusing, but it doesn’t have to
+                be. Take a look at this video to learn how you can get started
+                with cryptocurrency.
               </h3>
               <NavLink>READ MORE</NavLink>
             </div>
           </div>
         </div>
       </div>
-      <div className='container-xl opct' data-aos='fade-up'>
+      <div className="container-xl opct" data-aos="fade-up">
         <h1>Where to Buy OPCT</h1>
-        <div className='row text-center align-items-center'>
-          <div className='col-md-4'>
-            <div className='card-item'>
+        <div className="row text-center align-items-center">
+          <div className="col-md-4">
+            <div className="card-item">
               <img src={uniswap} />
               <NavLink>Buy OPCT on UNISWAP</NavLink>
             </div>
           </div>
-          <div className='col-md-4'>
-            <div className='card-item'>
+          <div className="col-md-4">
+            <div className="card-item">
               <img src={kucoin} />
               <NavLink>Buy OPCT on KUCOIN</NavLink>
             </div>
           </div>
-          <div className='col-md-4'>
-            <div className='card-item'>
+          <div className="col-md-4">
+            <div className="card-item">
               <img src={mercatox} />
               <NavLink>Buy OPCT on MERCATOX</NavLink>
             </div>
