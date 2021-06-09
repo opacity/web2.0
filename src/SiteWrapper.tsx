64d295f00c8d1fbb0e85bfd79638837b6eed3fe5
Jesus Espitia <jesus@opacity.io>
2021-06-08 18:58:02 -0700

    DEV-26: Change link "App Gallery" to "Downloads"

--- src/SiteWrapper.tsx
+++ src/SiteWrapper.tsx
@@ -3,7 +3,14 @@
 import * as React from "react";
 import { ReactElement, ReactNode } from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import { Site, Nav, Grid, List, Button, RouterContextProvider } from "tabler-react";
+import {
+  Site,
+  Nav,
+  Grid,
+  List,
+  Button,
+  RouterContextProvider,
+} from "tabler-react";
 import AOS from "aos";
 import type { NotificationProps } from "tabler-react";
 import { Navbar, NavDropdown } from "react-bootstrap";
@@ -66,9 +73,9 @@ class SiteWrapper extends React.Component<Props, State> {
   }
 
   render(): ReactElement {
-    const loggedIn = localStorage.getItem('key') ? true : false;
+    const loggedIn = localStorage.getItem("key") ? true : false;
     return (
-      <div className='page'>
+      <div className="page">
         <header
           className={
             this.props.isHome
@@ -76,151 +83,150 @@ class SiteWrapper extends React.Component<Props, State> {
               : "navbar navbar-expand-md navbar-light d-print-none border-bottom"
           }
         >
-          <div className='container-xl'>
-            <h1 className='navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3 mb-0'>
-              <NavLink to='/' className='logo-wrapper'>
-                <img src={logo} width='60' height='60' alt='Opacity' className='navbar-brand-image' />
-                <span className='ml-3'>OPACITY</span>
+          <div className="container-xl">
+            <h1 className="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3 mb-0">
+              <NavLink to="/" className="logo-wrapper">
+                <img
+                  src={logo}
+                  width="60"
+                  height="60"
+                  alt="Opacity"
+                  className="navbar-brand-image"
+                />
+                <span className="ml-3">OPACITY</span>
               </NavLink>
             </h1>
             <button
-              className='navbar-toggler'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#navbar-menu'
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbar-menu"
               aria-expanded={this.state.showMobileMenu}
               onClick={this.SetMobileMenu.bind(this)}
             >
-              <span className='navbar-toggler-icon'></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className='collapse navbar-collapse' id='navbar-menu'>
-              <div className='d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center justify-content-end'>
-                <ul className='navbar-nav'>
-                  <li className='nav-item'>
-                    <NavLink to='/platform' className='nav-link'>
+            <div className="collapse navbar-collapse" id="navbar-menu">
+              <div className="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center justify-content-end">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <NavLink to="/platform" className="nav-link">
                       Why Opacity?
                     </NavLink>
                   </li>
-                  <li className='nav-item'>
-                    <NavLink to='/community' className='nav-link'>
-                      App Gallery
+                  <li className="nav-item">
+                    <NavLink to="/downloads" className="nav-link">
+                      Downloads
                     </NavLink>
                   </li>
-                  <li className='nav-item'>
-                    <Nav.Link href='/blog'>Learn</Nav.Link>
+                  <li className="nav-item">
+                    <Nav.Link href="/blog">Learn</Nav.Link>
                   </li>
-                  <li className='nav-item'>
-                    {loggedIn ?
-                      (
-                        <div className='nav-link'>
-                          <Button
-                            className='btn btn-primary'
-                            onClick={() => {
-                              this.props.history.push("/file-manager");
-                            }}
-                          >
-                            Dashboard
-                        </Button>
-                        </div>
-                      ) : (
-                        <div className='nav-link'>
-                          <Button
-                            className='btn btn-white btn-pill'
-                            onClick={() => {
-                              this.props.history.push("/plans");
-                            }}
-                          >
-                            Explore Plans
-                      </Button>
-                        </div>
-                      )
-                    }
-
-                  </li>
-                  <li className='nav-item'>
+                  <li className="nav-item">
                     {loggedIn ? (
-                      <div className=''>
+                      <div className="nav-link">
                         <Button
-                          className='btn btn-primary'
+                          className="btn btn-primary"
+                          onClick={() => {
+                            this.props.history.push("/file-manager");
+                          }}
+                        >
+                          Dashboard
+                        </Button>
+                      </div>
+                    ) : (
+                      <div className="nav-link">
+                        <Button
+                          className="btn btn-white btn-pill"
+                          onClick={() => {
+                            this.props.history.push("/plans");
+                          }}
+                        >
+                          Explore Plans
+                        </Button>
+                      </div>
+                    )}
+                  </li>
+                  <li className="nav-item">
+                    {loggedIn ? (
+                      <div className="">
+                        <Button
+                          className="btn btn-primary"
                           onClick={() => {
                             localStorage.clear();
-                            this.props.history.push('/')
+                            this.props.history.push("/");
                           }}
                         >
                           Logout
                         </Button>
                       </div>
                     ) : (
-                        <div className=''>
-                          <Button
-                            className='btn btn-primary btn-pill'
-                            onClick={() => {
-                              this.setState({ showLoginModal: true });
-                            }}
-                          >
-                            Log in
-                      </Button>
-                        </div>
-                      )}
-
+                      <div className="">
+                        <Button
+                          className="btn btn-primary btn-pill"
+                          onClick={() => {
+                            this.setState({ showLoginModal: true });
+                          }}
+                        >
+                          Log in
+                        </Button>
+                      </div>
+                    )}
                   </li>
                 </ul>
               </div>
             </div>
             {this.state.showMobileMenu && (
-              <div className='mobile-menu'>
-                <div className='d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center justify-content-center'>
-                  <ul className='navbar-nav'>
-                    <li className='nav-item'>
-                      <NavLink to='/platform' className='nav-link'>
+              <div className="mobile-menu">
+                <div className="d-flex flex-column flex-md-row flex-fill align-items-stretch align-items-md-center justify-content-center">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <NavLink to="/platform" className="nav-link">
                         Why Opacity?
-                    </NavLink>
+                      </NavLink>
                     </li>
-                    <li className='nav-item'>
-                      <NavLink to='/community' className='nav-link'>
-                        App Gallery
-                    </NavLink>
+                    <li className="nav-item">
+                      <NavLink to="/downloads" className="nav-link">
+                        Downloads
+                      </NavLink>
                     </li>
-                    <li className='nav-item'>
-                      <Nav.Link href='/blog'>Learn</Nav.Link>
+                    <li className="nav-item">
+                      <Nav.Link href="/blog">Learn</Nav.Link>
                     </li>
 
-                    {
-                      loggedIn ? (
-                        <>
-                          <li className='nav-item'>
-                            <NavLink to='/file-manager' className='nav-link'>
-                              Dashboard
-                              </NavLink>
-                          </li>
-                          <li
-                            className='nav-item'
-                            onClick={() => {
-                              localStorage.clear();
-                              this.props.history.push('/')
-                            }}
-                          >
-                            <Nav.Link>Logout</Nav.Link>
-                          </li>
-                        </>
-                      ) : (
-                          <>
-                            <li className='nav-item'>
-                              <Nav.Link href='/plans'>Explore Plans</Nav.Link>
-                            </li>
-                            <li
-                              className='nav-item'
-                              onClick={() => {
-                                this.setState({ showLoginModal: true });
-                              }}
-                            >
-                              <Nav.Link>Log in</Nav.Link>
-                            </li>
-                          </>
-                        )
-                    }
-
+                    {loggedIn ? (
+                      <>
+                        <li className="nav-item">
+                          <NavLink to="/file-manager" className="nav-link">
+                            Dashboard
+                          </NavLink>
+                        </li>
+                        <li
+                          className="nav-item"
+                          onClick={() => {
+                            localStorage.clear();
+                            this.props.history.push("/");
+                          }}
+                        >
+                          <Nav.Link>Logout</Nav.Link>
+                        </li>
+                      </>
+                    ) : (
+                      <>
+                        <li className="nav-item">
+                          <Nav.Link href="/plans">Explore Plans</Nav.Link>
+                        </li>
+                        <li
+                          className="nav-item"
+                          onClick={() => {
+                            this.setState({ showLoginModal: true });
+                          }}
+                        >
+                          <Nav.Link>Log in</Nav.Link>
+                        </li>
+                      </>
+                    )}
                   </ul>
                 </div>
               </div>
@@ -237,20 +243,22 @@ class SiteWrapper extends React.Component<Props, State> {
           }}
           handleClose={() => {
             this.handleCloseLoginModal();
-            this.props.handleCloseLoginModal && this.props.handleCloseLoginModal();
+            this.props.handleCloseLoginModal &&
+              this.props.handleCloseLoginModal();
           }}
         />
-        { (this.state.showSignUpModal || this.props.showSignUpModal) &&
+        {(this.state.showSignUpModal || this.props.showSignUpModal) && (
           <SignUpModal
             show={this.state.showSignUpModal || this.props.showSignUpModal}
             handleClose={() => {
               this.setState({ showSignUpModal: false });
-              this.props.handleCloseSignUpModal && this.props.handleCloseSignUpModal();
+              this.props.handleCloseSignUpModal &&
+                this.props.handleCloseSignUpModal();
             }}
             plan={this.props.plan}
             openLoginModal={this.handleOpenLoginModal.bind(this)}
           />
-        }
+        )}
       </div>
     );
   }
