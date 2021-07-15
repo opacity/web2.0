ea535037599c58bdc58ec3ed3923748fabb87cb0
supermavens <sunavenue.ca@gmail.com>
2021-07-15 06:31:12 -0700

    testing metamask issue

--- src/services/metamask.ts
+++ src/services/metamask.ts
@@ -1,4 +1,5 @@
 // import opacityABI from "../contracts/opacity.abi.json";
+import Web3 from 'web3';
 
 const CONTRACT_ADDRESS = "0xDb05EA0877A2622883941b939f0bb11d1ac7c400";
 
@@ -9,7 +10,7 @@ declare global {
 }
 
 const isNewVersion = !!window.ethereum;
-const isInstalled = isNewVersion ;
+const isInstalled = isNewVersion;
 
 const fetchDefaultMetamaskAccount = async () => {
   if (isNewVersion) {
@@ -22,21 +23,51 @@ const fetchDefaultMetamaskAccount = async () => {
 
 const sendTransaction = ({ cost, from, to, gasPrice }) =>
   new Promise((resolve, reject) => {
+    const web3 = new Web3(window.ethereum);
     window.ethereum
-    .request({
-      method: 'eth_sendTransaction',
-      params: [
-        {
-          from,
-          to,
-          value: window.ethereum.Web3.Web3.Convert.ToWei(gasPrice, "ether"),
-          gasPrice: window.ethereum.Web3.Web3.Convert.ToWei(gasPrice, "gwei"),
-          gas: '6000',
-        },
-      ],
-    })
-    .then((txHash) => resolve(txHash))
-    .catch((error) => reject(error));
+      .request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from,
+            to,
+            gas: '0xEA60',
+            value: web3.utils.toWei(cost.toString(), "ether"),
+          },
+        ],
+      })
+      .then((txHash) => resolve(txHash))
+      .catch((error) => reject(error));
+  // });
+  // new Promise((resolve, reject) => {
+  //   const web3 = new Web3(window.ethereum);
+
+  //   const opacityContract = new web3.eth.Contract(
+  //     opacityABI,
+  //     CONTRACT_ADDRESS,
+  //     {
+  //       from,
+  //     })
+
+  //   web3.eth.sendTransaction({
+  //     from,
+  //     to,
+  //     value: web3.utils.toWei(cost.toString(), "ether"),
+  //     gas: 60000,
+  //   },
+  //     (err, res) => {
+  //       err ? reject(err) : resolve(res);
+  //     })
+
+    // opacityContract.methods.myMethod(123).send(
+    //   {
+    //     from,
+    //     gas: 60000,
+    //   },
+    //   (err, res) => {
+    //     err ? reject(err) : resolve(res);
+    //   }
+    // );
   });
 
 export default {
