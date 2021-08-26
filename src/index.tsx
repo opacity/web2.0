48ef597027c10908f1216c121d0a330193e39a51
Mihai Iorga <mihai7221@gmail.com>
2021-08-26 18:15:32 +0300

    Sentry tracing only on backend

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,7 @@ import { store, persistor } from "./redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
-import { VERSION, IS_LOCAL, IS_DEV, HOST } from "./config";
+import { VERSION, IS_LOCAL, IS_DEV, DEFAULT_STORAGE_NODE_IP } from "./config";
 
 let sentryOptions = {
   dsn: "https://8fdbdab452f04a43b5c3f2e00ec126f7@sentry.io/295597",
@@ -33,11 +33,12 @@ let sentryOptions = {
   integrations: [
     new Integrations.BrowserTracing({
       routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-      tracingOrigins: [HOST],
+      tracingOrigins: [DEFAULT_STORAGE_NODE_IP],
     }),
   ],
   tracesSampleRate: 0.3,
 }
+
 if (IS_DEV) {
   sentryOptions.tracesSampleRate = 1;
 }
