798802125442a335920897750980d341d82b93f4
Jesus Espitia <jesus@opacity.io>
2021-11-02 12:36:33 -0700

    Revert If statement

--- src/components/WarningWrapper/WarningWrapper.tsx
+++ src/components/WarningWrapper/WarningWrapper.tsx
@@ -4,24 +4,21 @@ import { Alert } from "tabler-react";
 import "./WarningWrapper.scss";
 
 const WarningWrapper = (props) => {
-  if (
-    window.location.hostname === "dev2.opacity.io" ||
-    window.location.hostname === "127.0.0.1" ||
-    window.location.hostname === "localhost"
-  )
-    return (
-      <>
-        <div className="full-width">
-          <Alert type="warning" className="margin-0 text-center">
-            This site is for test purposes only. If you are interested in a full featured Opacity Storage account, please visit
-            www.opacity.io.
-          </Alert>
-        </div>
-        <div className="full-width">{props.children}</div>
-      </>
-    );
+  if (window.location.hostname === "www.opacity.io" || window.location.hostname === "opacity.io") {
+    return <>{props.children}</>;
+  }
 
-  return <>{props.children}</>;
+  return (
+    <>
+      <div className="full-width">
+        <Alert type="warning" className="margin-0 text-center">
+          This site is for test purposes only. If you are interested in a full featured Opacity Storage account, please visit
+          www.opacity.io.
+        </Alert>
+      </div>
+      <div className="full-width">{props.children}</div>
+    </>
+  );
 };
 
 export default WarningWrapper;
