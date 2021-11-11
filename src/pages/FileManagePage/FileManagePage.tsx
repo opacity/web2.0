e64e9cba302000fbed69dfb7511a4c83efee0ae5
K.Truong <66924285+supermaven@users.noreply.github.com>
2021-11-11 14:26:21 -0400

    fixed issue on sorting file and folder with corrupted accounts (#250)

--- src/pages/FileManagePage/FileManagePage.tsx
+++ src/pages/FileManagePage/FileManagePage.tsx
@@ -1072,9 +1072,14 @@ const FileManagePage = ({ history }) => {
     setPageLoading(true);
     loadingFlagCnt++;
     const metaList = fileList.map(async (file) => {
-      return await accountSystem._getFileMetadata(file.location).then((f) => {
-        return f;
-      });
+      return await accountSystem
+        ._getFileMetadata(file.location)
+        .then((f) => {
+          return f;
+        })
+        .catch(() => {
+          return file;
+        });
     });
     const tmp = await Promise.all(metaList);
     setFileMetaList(tmp);
@@ -1087,9 +1092,14 @@ const FileManagePage = ({ history }) => {
     setPageLoading(true);
     loadingFlagCnt++;
     const metaList = folderList.map(async (folder) => {
-      return await accountSystem._getFolderMetadataByLocation(folder.location).then((f) => {
-        return f;
-      });
+      return await accountSystem
+        ._getFolderMetadataByLocation(folder.location)
+        .then((f) => {
+          return f;
+        })
+        .catch(() => {
+          return folder;
+        });
     });
     const tmp = await Promise.all(metaList);
     setFolderMetaList(tmp);
